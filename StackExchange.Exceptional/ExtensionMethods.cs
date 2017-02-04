ad8c737544ae817442cffeda3dd2f680c031d54a
Raph <raphael.ducom@gmail.com>
2017-02-04 17:15:53 +0100

    removes IPv4 regex

--- StackExchange.Exceptional/ExtensionMethods.cs
+++ StackExchange.Exceptional/ExtensionMethods.cs
@@ -151,9 +151,7 @@ namespace StackExchange.Exceptional.Extensions
         /// When a client IP can't be determined
         /// </summary>
         public const string UnknownIP = "0.0.0.0";
-
-        private static readonly Regex IPv4Regex = new Regex(@"\b([0-9]{1,3}\.){3}[0-9]{1,3}$", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
-
+        
         /// <summary>
         /// returns true if this is a private network IP  
         /// http://en.wikipedia.org/wiki/Private_network
@@ -216,12 +214,8 @@ namespace StackExchange.Exceptional.Extensions
             var ip = serverVariables["REMOTE_ADDR"]; // could be a proxy -- beware
             var ipForwarded = serverVariables["HTTP_X_FORWARDED_FOR"];
             // check if we were forwarded from a proxy
-            if (ipForwarded.HasValue())
-            {
-                ipForwarded = IPv4Regex.Match(ipForwarded).Value;
-                if (ipForwarded.HasValue() && !IsPrivateIP(ipForwarded))
-                    ip = ipForwarded;
-            }
+            if (ipForwarded.HasValue() && !IsPrivateIP(ipForwarded))
+                ip = ipForwarded;
 
             return ip.HasValue() ? ip : UnknownIP;
         }
