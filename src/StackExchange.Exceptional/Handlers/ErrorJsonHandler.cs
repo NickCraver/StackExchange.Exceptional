091ad70aadc5fc9c99367fec69804d4178457d8a
Nick Craver <nrcraver@gmail.com>
2017-07-23 20:28:55 -0400

    JSON error handler: eh, screw the custom object, v2 baby.

--- src/StackExchange.Exceptional/Handlers/ErrorJsonHandler.cs
+++ src/StackExchange.Exceptional/Handlers/ErrorJsonHandler.cs
@@ -15,43 +15,16 @@ namespace StackExchange.Exceptional.Handlers
         {
             context.Response.ContentType = "application/json";
 
-            DateTime since = long.TryParse(context.Request["since"], out long sinceLong)
+            DateTime? since = long.TryParse(context.Request["since"], out long sinceLong)
                      ? new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(sinceLong)
-                     : DateTime.MinValue;
+                     : (DateTime?)null;
 
             var errors = ErrorStore.Default.GetAll();
-            var result = errors.Where(error => error.CreationDate >= since).Select(error => new JsonError(error)).ToList();
-
-            serializer.Serialize(context.Response.Output, result);
-        }
-
-        private class JsonError
-        {
-            public string HostName { get; }
-            public string Message { get; }
-            public int DuplicateCount { get; }
-            public long EpochTime { get; }
-            public string Id { get; }
-            public string Type { get; }
-            public string IP { get; }
-            public string Host { get; }
-            public string Url { get; }
-            public bool Protected { get; }
-            public Dictionary<string, string> CustomData { get; }
-            public JsonError(Error error)
+            if (since.HasValue)
             {
-                Id = error.Id.ToString();
-                Message = error.Message;
-                DuplicateCount = error.DuplicateCount ?? 0;
-                EpochTime = error.CreationDate.ToEpochTime();
-                Type = error.Type;
-                IP = error.IPAddress;
-                Host = error.Host;
-                Url = error.Url;
-                Protected = error.IsProtected;
-                HostName = error.MachineName;
-                CustomData = error.CustomData;
+                errors = errors.Where(error => error.CreationDate >= since).ToList();
             }
+            serializer.Serialize(context.Response.Output, errors);
         }
 
         public bool IsReusable => true;
