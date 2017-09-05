ef36f3fbaa8eef2f1015a4351624addbfe2237cf
Nick Craver <nrcraver@gmail.com>
2017-09-05 16:51:34 -0400

    Add Exceptional global observers
    These are conveinence methods for logging exceptions in background and global scenarios.

--- src/StackExchange.Exceptional.Shared/Exceptional.cs
+++ src/StackExchange.Exceptional.Shared/Exceptional.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace StackExchange.Exceptional
 {
     /// <summary>
     /// Central settings config point for context-less logging.
     /// </summary>
-    public class Exceptional
+    public static class Exceptional
     {
         /// <summary>
         /// Settings for context-less logging.
@@ -20,14 +21,54 @@ namespace StackExchange.Exceptional
         /// Initializes this settings store.
         /// </summary>
         /// <param name="settings"></param>
-        public static void Configure(SettingsBase settings) => 
+        public static void Configure(SettingsBase settings) =>
             Settings = settings ?? throw new ArgumentNullException(nameof(settings));
 
         /// <summary>
         /// Sets the default error store to use for logging
         /// </summary>
         /// <param name="store">The error store used to store, e.g. <code>new SQLErrorStore(myConnectionString)</code></param>
-        public static void Configure(ErrorStore store) => 
+        public static void Configure(ErrorStore store) =>
             Settings.DefaultStore = store ?? throw new ArgumentNullException(nameof(store));
+
+        private static readonly EventHandler<UnobservedTaskExceptionEventArgs> taskHandler = (s, args) =>
+        {
+            foreach (var ex in args.Exception.InnerExceptions)
+            {
+                ex.LogNoContext(rollupPerServer: true);
+            }
+            args.SetObserved();
+        };
+
+        /// <summary>
+        /// Attaches to <see cref="TaskScheduler.UnobservedTaskException"/> and handles all <see cref="Task"/> background exceptions.
+        /// Without handling these (or at least observing them) the default behavior is an application pool crashing.
+        /// </summary>
+        public static void ObserveTaskExceptions()
+        {
+            // Ensure we don't register twice
+            TaskScheduler.UnobservedTaskException -= taskHandler;
+            TaskScheduler.UnobservedTaskException += taskHandler;
+        }
+
+        private static readonly UnhandledExceptionEventHandler domainHandler = (s, args) =>
+        {
+            // args.ExceptionObject may not be an exception, refer to http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-335.pdf
+            // section 10.5, CLS Rule 40 if you're curious on why this check needs to happen
+            if (args.ExceptionObject is Exception e)
+            {
+                e.AddLogData("Terminating", args.IsTerminating).LogNoContext();
+            }
+        };
+
+        /// <summary>
+        /// Attaches to <see cref="AppDomain.UnhandledException"/> and handles all global exceptions.
+        /// This is useful for console applications where not observing the exception as part of the ASP.NET pipeline.
+        /// </summary>
+        public static void ObserveAppDomainUnhandledExceptions()
+        {
+            AppDomain.CurrentDomain.UnhandledException -= domainHandler;
+            AppDomain.CurrentDomain.UnhandledException += domainHandler;
+        }
     }
 }
