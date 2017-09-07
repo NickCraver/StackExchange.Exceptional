3fe71d656a71cd01f4eb275ab6a517ffbd370ad0
Nick Craver <nrcraver@gmail.com>
2017-09-06 22:03:54 -0400

    Add intitial tests
    We want to test advanced binding (and ensure it keeps working), good a time as any to start the test suite.
    
    However, this is broken due to SqlClient because who the fuck knows runtime BS. This is one of those very simple scenarios that should JUST WORK, and it still doesn't in 2.0, on the latest packages.

---
+++ tests/StackExchange.Exceptional.Tests.AspNetCore/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Exceptional.Tests.AspNetCore
+{
+    public class Configuration : BaseTest
+    {
+        public Configuration(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void FullBinding()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("test.json")
+                .Build();
+
+            Assert.NotNull(config);
+            var section = config.GetSection("Exceptional");
+            Assert.NotNull(section);
+            Assert.Equal("Exceptional", section.Key);
+
+            var settings = new ExceptionalSettings();
+            config.Bind(settings);
+            Assert.Equal(settings.Store.ApplicationName, $"Samples (ASP.NET Core)");
+        }
+    }
+}
