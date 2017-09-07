301a5c052807dc57a1551d46bf7c5cf87318603e
Nick Craver <nrcraver@gmail.com>
2017-09-06 22:03:54 -0400

    Add intitial tests
    We want to test advanced binding (and ensure it keeps working), good a time as any to start the test suite.
    
    However, this is broken due to SqlClient because who the fuck knows runtime BS. This is one of those very simple scenarios that should JUST WORK, and it still doesn't in 2.0, on the latest packages.

---
+++ tests/StackExchange.Exceptional.Tests.AspNetCore/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit.Abstractions;
+
+namespace StackExchange.Exceptional.Tests.AspNetCore
+{
+    public abstract class BaseTest
+    {
+        public const string NonParallel = nameof(NonParallel);
+
+        protected ITestOutputHelper Output { get; }
+
+        protected BaseTest(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+    }
+}
