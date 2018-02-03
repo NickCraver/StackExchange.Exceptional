20a618b3f6ba252d3a93028d48043cf0b49a1c37
Nick Craver <nrcraver@gmail.com>
2018-02-02 21:49:21 -0500

    Drop net451 support and move .Bind() for config down to .Shared
    This resolves #117 but more important cuts off net451 support now. It's simply not reasonable going forward (as it isn't covered by netstandard2.0) and it's best to do that before this major release. I know it sucks for some people on old systems but the reality is Microsoft doesn't support it either and we're dropping support for it across the board in all libraries due to "can't use netstandard2.0 only refs".
    
    On the .Bind() front, this adds support for confguration binding to non-ASP.NET Core scenarios, so yay for that.

--- src/StackExchange.Exceptional.AspNetCore/ExceptionalSettings.cs
+++ src/StackExchange.Exceptional.AspNetCore/ExceptionalSettings.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using StackExchange.Exceptional.Internal;
-using StackExchange.Exceptional.Notifiers;
 using System;
-using System.Text.RegularExpressions;
 
 namespace StackExchange.Exceptional
 {
@@ -22,46 +19,5 @@ namespace StackExchange.Exceptional
         /// but may need to be replaced in special multi-proxy situations.
         /// </summary>
         public Func<HttpContext, string> GetIPAddress { get; set; } = context => context.Connection.RemoteIpAddress.ToString();
-    }
-
-    /// <summary>
-    /// Extension methods for <see cref="ExceptionalSettings"/>.
-    /// </summary>
-    public static class ExceptionalSettingsExtensions
-    {
-        /// <summary>
-        /// Binds an <see cref="IConfiguration"/> to an <see cref="ExceptionalSettings"/> object.
-        /// This happens with a normal .Bind() followed by the complex type mappings manually.
-        /// </summary>
-        /// <param name="config">The <see cref="IConfigurationSection"/> to bind.</param>
-        /// <param name="settings">The <see cref="ExceptionalSettings"/> to bind to.</param>
-        public static void Bind(this IConfiguration config, ExceptionalSettings settings)
-        {
-            // Bind the simple types (almost everything)
-            ConfigurationBinder.Bind(config, settings); // because we overrode .Bind() here
-
-            // Now, explicitly bind the complex types
-            var dataIncludePattern = config.GetValue<string>(nameof(ExceptionalSettings.DataIncludeRegex));
-            if (!string.IsNullOrEmpty(dataIncludePattern))
-            {
-                settings.DataIncludeRegex = new Regex(dataIncludePattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
-            }
-
-            var ignoreRegexes = config.GetSection(nameof(ExceptionalSettings.Ignore))
-                                .GetSection(nameof(ExceptionalSettingsBase.IgnoreSettings.Regexes))
-                                .AsEnumerable();
-            foreach (var ir in ignoreRegexes)
-            {
-                if (ir.Value != null)
-                {
-                    settings.Ignore.Regexes.Add(new Regex(ir.Value, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline));
-                }
-            }
-            // If email is configured, hook it up
-            if (settings.Email.ToAddress.HasValue())
-            {
-                settings.Register(new EmailNotifier(settings.Email));
-            }
-        }
     }
 }
---
+++ src/StackExchange.Exceptional.Shared/ExceptionalSettingsExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Exceptional.Internal;
+using StackExchange.Exceptional.Notifiers;
+
+namespace StackExchange.Exceptional
+{
+    /// <summary>
+    /// Extension methods for <see cref="ExceptionalSettingsBase"/>.
+    /// </summary>
+    public static class ExceptionalSettingsExtensions
+    {
+        /// <summary>
+        /// Binds an <see cref="IConfiguration"/> to an <typeparamref name="T"/> object.
+        /// This happens with a normal .Bind() followed by the complex type mappings manually.
+        /// </summary>
+        /// <typeparam name="T">The specific type of <see cref="ExceptionalSettingsBase"/> to bind to.</typeparam>
+        /// <param name="config">The <see cref="IConfigurationSection"/> to bind.</param>
+        /// <param name="settings">The <typeparamref name="T"/> to bind to.</param>
+        public static void Bind<T>(this IConfiguration config, T settings) where T : ExceptionalSettingsBase
+        {
+            // Bind the simple types (almost everything)
+            ConfigurationBinder.Bind(config, settings); // because we overrode .Bind() here
+
+            // Now, explicitly bind the complex types
+            var dataIncludePattern = config.GetValue<string>(nameof(ExceptionalSettingsBase.DataIncludeRegex));
+            if (!string.IsNullOrEmpty(dataIncludePattern))
+            {
+                settings.DataIncludeRegex = new Regex(dataIncludePattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            }
+
+            var ignoreRegexes = config.GetSection(nameof(ExceptionalSettingsBase.Ignore))
+                                .GetSection(nameof(ExceptionalSettingsBase.IgnoreSettings.Regexes))
+                                .AsEnumerable();
+            foreach (var ir in ignoreRegexes)
+            {
+                if (ir.Value != null)
+                {
+                    settings.Ignore.Regexes.Add(new Regex(ir.Value, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline));
+                }
+            }
+            // If email is configured, hook it up
+            if (settings.Email.ToAddress.HasValue())
+            {
+                settings.Register(new EmailNotifier(settings.Email));
+            }
+        }
+    }
+}
