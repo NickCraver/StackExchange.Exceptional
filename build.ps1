ffa91b91e4ed4349322d65a67254d82c8c376023
Nick Craver <nrcraver@gmail.com>
2018-02-10 17:51:18 -0500

    Move to RC2 and with Nerdbank.GitVersioning
    Props to @onovotny for the push and help here, I'm digging it.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,5 @@
 [CmdletBinding(PositionalBinding=$false)]
 param(
-    [string] $Version,
-    [string] $BuildNumber,
     [bool] $CreatePackages,
     [bool] $RunTests = $true,
     [string] $PullRequestNumber
@@ -11,38 +9,9 @@ if ($BuildNumber -and $BuildNumber.Length -lt 5) {
     $BuildNumber = $BuildNumber.PadLeft(5, "0")
 }
 
-function CalculateVersion() {
-    if ($version) {
-        return $version
-    }
-
-    $semVersion = '';
-    $path = $pwd;
-    while (!$semVersion) {
-        if (Test-Path (Join-Path $path "semver.txt")) {
-            $semVersion = Get-Content (Join-Path $path "semver.txt")
-            break
-        }
-        if ($PSScriptRoot -eq $path) {
-            break
-        }
-        $path = Split-Path $path -Parent
-    }
-
-    if (!$semVersion) {
-        Write-Error "semver.txt was not found in $pwd or any parent directory"
-        Exit 1
-    }
-
-    return "$semVersion-$BuildNumber"
-}
-
 Write-Host "Run Parameters:" -ForegroundColor Cyan
-Write-Host "  Version: $Version"
-Write-Host "  BuildNumber: $BuildNumber"
 Write-Host "  CreatePackages: $CreatePackages"
 Write-Host "  RunTests: $RunTests"
-Write-Host "  Calculated Version (Global): $(CalculateVersion)"
 
 $packageOutputFolder = "$PSScriptRoot\.nupkgs"
 $projectsToBuild =
@@ -55,13 +24,6 @@ $projectsToBuild =
 $testsToRun =
 	'StackExchange.Exceptional.Tests',
 	'StackExchange.Exceptional.Tests.AspNetCore'
-
-if (!$Version -and !$BuildNumber) {
-    Write-Host "ERROR: You must supply either a -Version or -BuildNumber argument. `
-  Use -Version `"4.0.0`" for explicit version specification, or `
-  Use -BuildNumber `"12345`" for generation using <semver.txt>-<buildnumber>" -ForegroundColor Yellow
-    Exit 1
-}
 
 if ($PullRequestNumber) {
     Write-Host "Building for a pull request (#$PullRequestNumber), skipping packaging." -ForegroundColor Yellow
@@ -100,18 +62,14 @@ foreach ($project in $projectsToBuild) {
 	
 	Push-Location ".\src\$project"
 
-    $semVer = CalculateVersion
-
-    Write-Host "  Restoring and packing $project... (Version:" -NoNewline -ForegroundColor "Magenta"
-    Write-Host $semVer -NoNewline -ForegroundColor "Cyan"
-    Write-Host ")" -ForegroundColor "Magenta"
+    Write-Host "  Restoring and packing $project..." -ForegroundColor "Magenta"
     
     $targets = "Restore"
     if ($CreatePackages) {
         $targets += ";Pack"
     }
 
-	dotnet msbuild "/t:$targets" "/p:Configuration=Release" "/p:Version=$semVer" "/p:PackageOutputPath=$packageOutputFolder" "/p:CI=true"
+	dotnet msbuild "/t:$targets" "/p:Configuration=Release" "/p:PackageOutputPath=$packageOutputFolder" "/p:CI=true"
 
 	Pop-Location
 
