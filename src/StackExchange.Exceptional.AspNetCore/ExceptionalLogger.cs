2c4dd4b88bca269e6ba820a98c90ce2294a02e6b
Nick Craver <nrcraver@gmail.com>
2018-06-23 18:16:32 -0400

    ILogger: add setting for severity and known EventId entries

--- src/StackExchange.Exceptional.AspNetCore/ExceptionalLogger.cs
+++ src/StackExchange.Exceptional.AspNetCore/ExceptionalLogger.cs
@@ -6,45 +6,35 @@ using Microsoft.Extensions.Options;
 
 namespace StackExchange.Exceptional
 {
-    class ExceptionalLogger : ILogger
+    internal class ExceptionalLogger : ILogger
     {
         private readonly string _category;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IOptions<ExceptionalSettings> _settings;
-        private readonly bool _ignored;
-        private static readonly HashSet<string> _ignoredCategories = new HashSet<string>
-        {
-            typeof(ExceptionalMiddleware).FullName, // exceptional middleware calls some ILogger stuff itself, ignore those calls)
-        };
 
         public ExceptionalLogger(string category, IOptions<ExceptionalSettings> settings, IHttpContextAccessor httpContextAccessor = null)
         {
             _category = category;
             _settings = settings;
             _httpContextAccessor = httpContextAccessor;
-            _ignored = _ignoredCategories.Contains(category);
         }
 
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            return null;
-        }
+        public IDisposable BeginScope<TState>(TState state) => null;
 
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return !_ignored;
-            // TODO compare against settings and add support for per-category log levels, or we can just leave it up to LogFilters to decide later
-        }
+        public bool IsEnabled(LogLevel logLevel) => _settings.Value.ILoggerLevel <= logLevel;
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            if (_ignored) return;
-            if (exception == null) return;
+            // Ignore non-exceptions and Exceptional events themselves
+            if (exception == null || (ExceptionalLoggingEvents.Min <=eventId.Id && eventId.Id <= ExceptionalLoggingEvents.Max))
+            {
+                return;
+            }
 
             var customData = new Dictionary<string, string>
             {
-                ["AspNetCore.LogLevel"] = logLevel + "",
-                ["AspNetCore.EventId.Id"] = eventId.Id + "",
+                ["AspNetCore.LogLevel"] = logLevel.ToString(),
+                ["AspNetCore.EventId.Id"] = eventId.Id.ToString(),
                 ["AspNetCore.EventId.Name"] = eventId.Name,
                 ["AspNetCore.Message"] = formatter(state, exception),
             };
