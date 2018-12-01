b8333505bb5baed270487805d140023e58deaad9
Nick Craver <nrcraver@gmail.com>
2018-12-01 07:55:05 -0500

    MongoDB: enable skipping

--- tests/StackExchange.Exceptional.Tests/Storage/MongoDBErrorStoreTest.cs
+++ tests/StackExchange.Exceptional.Tests/Storage/MongoDBErrorStoreTest.cs
@@ -9,11 +9,16 @@ namespace StackExchange.Exceptional.Tests.Storage
 {
     public class MongoDBErrorStoreTest : StoreBaseTest, IClassFixture<MongoDBFixture>
     {
+        public string ConnectionString => TestConfig.Current.MongoDBConnectionString;
         private MongoDBFixture Fixture { get; }
 
         public MongoDBErrorStoreTest(MongoDBFixture fixture, ITestOutputHelper output) : base(output)
         {
             Fixture = fixture;
+            if (Fixture.ShouldSkip)
+            {
+                Skip.Inconclusive("Couldn't test against: " + ConnectionString + "\n" + fixture.SkipReason);
+            }
         }
 
         protected override ErrorStore GetStore([CallerMemberName]string appName = null) =>
@@ -27,6 +32,8 @@ namespace StackExchange.Exceptional.Tests.Storage
 
     public class MongoDBFixture : IDisposable
     {
+        public bool ShouldSkip { get; }
+        public string SkipReason { get; }
         public string ConnectionString { get; }
         public string TableName { get; }
 
@@ -35,13 +42,31 @@ namespace StackExchange.Exceptional.Tests.Storage
             Skip.IfNoConfig(nameof(TestConfig.Current.MongoDBConnectionString), TestConfig.Current.MongoDBConnectionString);
             ConnectionString = TestConfig.Current.MongoDBConnectionString;
             TableName = "Test" + Guid.NewGuid().ToString("N").Substring(24);
+            try
+            {
+                var databaseName = new MongoUrl(ConnectionString).DatabaseName;
+                var collection = new MongoClient(ConnectionString).GetDatabase(databaseName);
+                collection.ListCollections();
+            }
+            catch (Exception e)
+            {
+                ShouldSkip = true;
+                SkipReason = e.Message;
+            }
         }
 
         public void Dispose()
         {
-            var databaseName = new MongoUrl(TestConfig.Current.MongoDBConnectionString).DatabaseName;
-            var collection = new MongoClient(TestConfig.Current.MongoDBConnectionString).GetDatabase(databaseName);
-            collection.DropCollection(TableName);
+            try
+            {
+                var databaseName = new MongoUrl(ConnectionString).DatabaseName;
+                var collection = new MongoClient(ConnectionString).GetDatabase(databaseName);
+                collection.DropCollection(TableName);
+            }
+            catch when (ShouldSkip)
+            {
+                // if we didn't error initially then we'll throw
+            }
         }
     }
 }
