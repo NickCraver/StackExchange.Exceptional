602c37b580ef4bbcfa762f2b50b7852752a90b0a
Jeremiah Peschka <jpeschka@stackoverflow.com>
2022-01-04 14:02:04 -0500

    Updating to log exception as an int

--- DBScripts/SqlServer.sql
+++ DBScripts/SqlServer.sql
@@ -3,13 +3,35 @@
     Run this script for creating the exceptions table
     It will also upgrade a V1 schema to V2, just run the full script.
 */
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE [TABLE_SCHEMA] = 'dbo' AND [TABLE_NAME] = 'ExceptionLevel')
+BEGIN
+    CREATE TABLE [dbo].[ExceptionLevel] (
+        [Id] int NOT NULL IDENTITY (1,1),
+        [Name] varchar(8) NOT NULL
+        CONSTRAINT PK_ExceptionLevel PRIMARY KEY CLUSTERED (Id)
+    );
+
+    SET IDENTITY_INSERT ExceptionLevel ON;
+
+    INSERT INTO ExceptionLevel (Id, Name)
+    VALUES (1, 'Trace'),
+           (2, 'Debug'),
+           (3, 'Info'),
+           (4, 'Warning'),
+           (5, 'Error'),
+           (6, 'Critical');
+
+    SET IDENTITY_INSERT ExceptionLevel OFF;
+END
+GO
+
 IF NOT EXISTS (SELECT 1 
                  FROM INFORMATION_SCHEMA.TABLES 
                 WHERE [TABLE_SCHEMA] = 'dbo'
                   AND [TABLE_NAME] = 'Exceptions')
 BEGIN
     CREATE TABLE [dbo].[Exceptions](
-        [Id] bigint NOT NULL Identity,
+        [Id] bigint NOT NULL IDENTITY,
         [GUID] uniqueidentifier NOT NULL,
         [ApplicationName] nvarchar(50) NOT NULL,
         [MachineName] nvarchar(50) NOT NULL,
@@ -30,8 +52,9 @@ BEGIN
         [DuplicateCount] int NOT NULL DEFAULT(1),
         [LastLogDate] datetime NULL,
         [Category] nvarchar(100) NULL,
-        [LogLevel] nvarchar(8) NOT NULL DEFAULT('Critical')
+        [ExceptionLevelId] int NOT NULL DEFAULT(6) CONSTRAINT [FK_ExceptionLevel] FOREIGN KEY REFERENCES dbo.ExceptionLevel(Id)
      CONSTRAINT [PK_Exceptions] PRIMARY KEY Clustered ([Id] ASC)
+     
      WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
     );
 END
@@ -95,8 +118,12 @@ BEGIN
     ALTER TABLE [dbo].[Exceptions] DROP COLUMN [SQL];
 END
 
-IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Exceptions' AND COLUMN_NAME = 'LogLevel')
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Exceptions' AND COLUMN_NAME = 'ExceptionLevelId')
 BEGIN
     ALTER TABLE [dbo].[Exceptions] 
-    ADD [LogLevel] NVARCHAR(8) NOT NULL DEFAULT('Critical');
+    ADD [ExceptionLevelId] int NOT NULL DEFAULT(6);
+
+    ALTER TABLE [dbo].[Exceptions]  WITH CHECK
+    ADD CONSTRAINT [FK_ExceptionLevel] FOREIGN KEY([ExceptionLevelId])
+    REFERENCES [dbo].[ExceptionLevel] ([Id]);
 END
